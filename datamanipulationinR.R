c8eb10c9dfaf1dadf1e2ec92b7853a17086db2b5
Carolina Giménez Arias <carolgimenezarias@MacBook-Air-de-Carolina-2.local>
2019-01-27 23:33:42 +0100

    Add chapter 1

---
+++ datamanipulationinR.R
@@ -0,0 +1,11 @@
+# Chapter 1
+
+# Load the dplyr package
+library(dplyr)
+
+# Load the hflights package
+library(hflights)
+
+# Call both head() and summary() on hflights
+head(hflights)
+summary(hflights)
